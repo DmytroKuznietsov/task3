0d3f62cae3f5ac0d1241b75a8f52e3d055fe7f86
Dmytro Kuznietsov <kuznietsov.workmail@gmail.com>
2019-09-20 16:08:41 +0300

    firs commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,120 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+//TASK 1,2,3
+ public class Main {
+
+
+
+    public static void main(String[] args) {
+       /* String st = ("Variable string type");
+        String lesson = ("Lesson 1");
+        int taskNo = 1;
+        System.out.println(lesson + ", Task " +taskNo);
+        System.out.println("Hello world");
+        System.out.println(st);
+        System.out.println("Task "+ taskNo + " is done!");
+
+
+        Boolean A1 = false;
+        byte A2 = 127;
+        char A3 = 99;
+        short A4 = 15;
+        int n = taskNo +1;
+        long A5 = -4;
+        float A6 = 23512;
+        double A7 = 123512340.834235;
+
+        int a = 12;
+        int b = 16;
+        int s = a*b;
+
+        System.out.println("Площадь четыриугольника со сторонами "+a+" см и "+b+" см ровняеться "+s);
+        System.out.println("Task "+ n + " is done!");*/
+
+       // TASK 4. Fix the program to get the correct result:
+
+
+
+      /* 4. Fix the program to get the correct result:
+
+                int a = 5;
+                int b = 10;
+                float result =(float)a / b;
+
+                System.out.println(result);
+            }
+        }
+        END TASK 4*/
+
+
+               /* int a = 1;
+                int b = 3;
+                int c = 9;
+                int d = 27;
+
+
+                int result = - a + b - c + d;
+
+                System.out.println(result);
+
+            }
+        }
+        END TASK 5*/
+            /*
+            String s = "Java";
+            //System.out.println("Happy");
+            //System.out.println("Java Learning");
+            //System.out.println("programming");
+            System.out.print("Happy Java");
+           // System.out.println("weekend");
+           // System.out.println(s);
+            //System.out.print("Python");
+            System.out.print(" ");
+            System.out.println("Learning");
+                }
+        }
+         END TASK 6*/
+
+
+                /*
+            int a = 10;
+            int b = 15;
+            double c = b + 38;
+            //int d = a + 12;
+            //double e = 12.3;
+            String s = "s" + a;
+            String s1 = a + "b";
+           // String s2 = "a";
+            String s3 = s1 + "a";
+            String s4 = s3 + "b";
+            System.out.println(c + s4 + s);
+        //}
+            }
+        }
+        END TASK 7*/
+        ArrayList<Character> characters = transformIntToChar(72,69,76,76,79,87,32,87,79,76,68);
+        printList(characters);
+
+       // int [] array1 = {72,69,76,76,79,87,32,87,79,76,68};
+    }
+            //int k = array1.length;
+           // System.out.println(k);
+
+
+        private static ArrayList<Character> transformIntToChar(int...values) {
+            ArrayList<Character> buffer = new ArrayList<>();  //Array List класс коллекция, шаблон итератор, тип ArrayList<Character> (тип внутри типа). 'New' - оператор, которрый выделяет место в памяти под значения, которые будут содержаться внутри ArrayList(класс) и возвращает ссылку на экземпляр. (состояние конкретного экземпляра класса)
+            for (int i: values) {   // цикл перебирает каждое значение в массиве values
+                buffer.add(((char) i));
+            }
+            return buffer;
+        }
+
+        private static void printList(ArrayList<Character> characters) {
+            for (char c: characters) {
+                System.out.print(c);
+            }
+        }
+
+    }
